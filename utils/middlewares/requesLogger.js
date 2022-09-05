24e7d5197b1d45c74918d8c2f8bc36748f00386c
achraf2020-code <achraf-saidi@outlook.com>
2022-09-06 01:29:42 +0200

    fixing requestLogger msg

--- utils/middlewares/requesLogger.js
+++ utils/middlewares/requesLogger.js
@@ -1,7 +1,6 @@
 const requestLogger = (req, res, next) => {
-  console.log("request header: ", req.header);
-  console.log("request method: ", req.method);
   console.log("request path: ", req.path);
+  console.log("request method: ", req.method);
   console.log("request body: ", req.body);
   console.log("----");
   next();
