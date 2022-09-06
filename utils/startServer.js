d714161dc745fd005038cf94f0301ee451d899fa
achraf2020-code <achraf-saidi@outlook.com>
2022-09-07 00:37:38 +0200

    fix port number in production

--- utils/startServer.js
+++ utils/startServer.js
@@ -3,13 +3,13 @@ const logger = require("./logger");
 const express = require("express");
 const app = express();
 const startServer = () => {
-  const server = app.listen(PORT, SERVERURI, function (error) {
+  const server = app.listen(PORT, function (error) {
     if (error) {
       logger.error(error);
     }
-    const host = server.address().address;
+    // const host = server.address().address;
     const port = server.address().port;
-    logger.info(`server is listening at http://${host}:${port}/`);
+    logger.info(`server is listening at port ${port}/`);
   });
 };
 module.exports = startServer;
